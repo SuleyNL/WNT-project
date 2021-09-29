2d604c72a7e18867652d7b96ad64659dd7e5f148
SuleyNL <Suleyleeuw@gmail.com>
2021-09-29 16:26:37 +0200

    added professional error enums
    added datetime to the report
    
    each pdf now has a report, also the ones with pages found
    
    eliminated the connectionLost bug by waiting up to 1 minute for each downloading pdf

--- PyDFScraper.py
+++ PyDFScraper.py
@@ -2,23 +2,25 @@ import os
 import math
 import requests
 import time
+import datetime
 import PyPDF2
 from PyPDF2 import PdfFileReader, PdfFileWriter, utils
 from pathlib import Path
 import pikepdf
 from pdfminer.pdfpage import PDFPage
 from reportlab.pdfgen.canvas import Canvas
-
+from enum import Enum
 
 def startProcess(year):
     global isError
-    isError = False
+    global report
     organisationAmount = getOrganisationAmount()
     iteration = getIteration(year)
 
     while iteration[0] < organisationAmount:
-
+        #   the default setting is the best case scenario, will be modified if something is wrong.
         isError = False
+        report = "Wel resultaten"
 
         iteration = getIteration(year)
         organisation = getOrganisation(iteration)
@@ -29,13 +31,25 @@ def startProcess(year):
             try:
                 pageNumber = getPageNumber()
             except PyPDF2.utils.PdfReadError as e:
-                errorhandler(e)
+                errorhandler(Error.EOFError)
 
         generateFile(pageNumber, year, organisation, iteration, url)
-        # Connection error if you let it go on immediately
-        print("waiting 5 minutes")
-        time.sleep(60 * 5)
     return "------[THE END]------"
+
+
+class Error(Enum):
+    noresultsError = "Geen resultaten"
+    encryptionError = "Encrypted"
+    EOFError = "EOF-Error"
+    fewresultsError = "Te weinig resultaten"
+    nodocumentError = "Geen document"
+
+
+def errorhandler(error):
+    global isError
+    global report
+    report = error.value
+    isError = True
 
 
 def getIteration(year):
@@ -84,7 +98,7 @@ def getUrl(coordinates, year):
         if not coordinates[1] == len(urlList):
             url = ":".join(line).split(", ")[coordinates[1]].strip()
         else:
-            errorhandler("nodocument")
+            errorhandler(Error.nodocumentError)
     return url
 
 
@@ -93,15 +107,20 @@ def isFileDownloaded(url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"}
 
     try:
-        response = requests.get(url, headers=headers)
-    except requests.exceptions.ConnectionError:
-        isFileDownloaded(url)
-        print("trying again")
-    path = Path("WorkingMemory")
-    path.mkdir(parents=True, exist_ok=True)
+        print("waiting up to 1 minute to download pdf from: %s" % url)
+        response = requests.get(url, headers=headers, timeout=60)
 
-    with open("WorkingMemory/currentFile.pdf", "wb+") as file:
-        file.write(response.content)
+
+        path = Path("WorkingMemory")
+        path.mkdir(parents=True, exist_ok=True)
+
+        with open("WorkingMemory/currentFile.pdf", "wb+") as file:
+            file.write(response.content)
+
+    except requests.exceptions.ConnectionError or UnboundLocalError:
+        print("error: ")
+        print("trying again")
+        isFileDownloaded(url)
     return True
 
 
@@ -122,27 +141,34 @@ def getPageNumber():
 
     totalScore = []
 
+    i = 0
     try:
-        while pageNumber < pdf.getNumPages():
-            page = pdf.getPage(pageNumber).extractText().split(" ")
+        while i < pdf.getNumPages():
+            page = pdf.getPage(i).extractText().split(" ")
 
             #relevanceScore = page.count("bezoldiging") + page.count("Bezoldiging") + page.count("BEZOLDIGING") + page.count("WNT")
             #print(item.count("bezoldiging") for item in page)
-            relevanceScore = sum((itm.count("bezoldiging") for itm in page)) + \
-                             sum((itm.count("WNT") for itm in page)) + \
-                             sum((itm.count("bezoldigingsmaximum") for itm in page)) + \
-                             sum((itm.count("Wet Normering") for itm in page))
+            highscore = 0
+            currentScore =  sum((itm.count("bezoldiging") for itm in page)) + \
+                            sum((itm.count("WNT") for itm in page)) + \
+                            sum((itm.count("bezoldigingsmaximum") for itm in page)) + \
+                            sum((itm.count("Wet Normering") for itm in page)) + \
+                            sum((itm.count("Bezoldiging") for itm in page))
 
-            totalScore.append(relevanceScore)
+            totalScore.append(currentScore)
+            if currentScore > highscore:
+                highscore = currentScore
+                pageNumber = highscore
+            i += 1
 
-            pageNumber += 1
     except:
-        errorhandler("encrypted")
+        errorhandler(Error.encryptionError)
 
-
-    # if there are no results for searchterm, activate error
+    # if there are no results for searchterm, report an error
     if sum(totalScore) < 1:
-        errorhandler("noresults")
+        errorhandler(Error.noresultsError)
+    elif sum(totalScore) < 4:
+        errorhandler(Error.fewresultsError)
     else:
         # Find the page number with the highest value
         pageNumber = totalScore.index(max(totalScore))
@@ -151,55 +177,68 @@ def getPageNumber():
     return pageNumber
 
 
-def errorhandler(errorText):
-    global isError
-    global error
-    error = errorText
-    isError = True
-
-
 def generateFile(pageNumber, year, organisation, iteration, url):
     global isError
-    global error
+    global report
+
+    #   create the file
+    newPath = Path("PDFs/{0}/{1}/".format(year, organisation))
+    newPath.mkdir(parents=True, exist_ok=True)
+    newFile = str(newPath) + "/{0}. {1}.pdf".format(iteration[1]+1, report)
+
+    #   create the first page, with a report of found results
+    canvas = Canvas(newFile)
+    errorText = report
+
+    canvas.drawString(20, 800, "Dit document heeft:")
+    canvas.drawString(180, 800, str(errorText))
+
+    canvas.drawString(20, 760, "Organisatie: ")
+    canvas.drawString(180, 760, organisation)
+
+    getMonthName = ["Januari",
+                     "Februari",
+                     "Maart",
+                     "April",
+                     "Mei",
+                     "Juni",
+                     "Juli",
+                     "Augustus",
+                     "September",
+                     "Oktober",
+                     "November",
+                     "December"]
+    today = datetime.datetime.strptime(str(datetime.date.today()), "%Y-%m-%d")
+
+    canvas.drawString(20, 720, "Moment van scraping: ")
+    canvas.drawString(180, 720, str(today.day) + " " + getMonthName[today.month-1] + " " + str(today.year))
+
+    canvas.drawString(20, 700, "")
+    canvas.drawString(180, 700, str(format(datetime.datetime.now(), '%H:%M:%S')))
+
+    canvas.drawString(20, 680, "Link: ")
+    canvas.drawString(20, 660, url)
+    rect = Canvas.rect(canvas, 20, 700, 400, 20, 0)
+    canvas.linkURL(url, rect)
+
+    canvas.setFont("Helvetica", 11)
+    canvas.save()
+
+    # if there is no error: we have located the relevant pages and can store them in this pdf
     if not isError:
         pages_to_keep = [pageNumber-1, pageNumber, pageNumber+1, pageNumber+2]
         infile = PdfFileReader("WorkingMemory/currentFile.pdf", 'rb')
         output = PdfFileWriter()
 
+        reportpage = PdfFileReader(newFile, 'rb').getPage(0)
+        output.addPage(reportpage)
         for i in pages_to_keep:
             p = infile.getPage(i)
             output.addPage(p)
 
-        # create path
-        newPath = Path("PDFs/{0}/{1}/".format(year, organisation))
-        newPath.mkdir(parents=True, exist_ok=True)
-        # create file
-        newFile = Path(str(newPath) + "/{0}.pdf".format(iteration[1]))
         with open(newFile, 'wb') as f:
             output.write(f)
-    else:
-        # create path to store PDF's in
-        print(organisation)
-        newPath = Path("PDFs/{0}/{1}/".format(year, organisation))
-        newPath.mkdir(parents=True, exist_ok=True)
 
-        # create special error file
-        newFile = str(newPath) + "/{0}.{1}.pdf".format(iteration[1], error)
-        canvas = Canvas(newFile)
-        if error == "noresults":
-            errorText = "geen resultaten"
-        else:
-            errorText = error
-        canvas.drawString(20, 800, "Dit document heeft:")
-        canvas.drawString(150, 800, str(errorText))
-        canvas.drawString(20, 760, "Organisatie: ")
-        canvas.drawString(150, 760, organisation)
-        canvas.drawString(20, 720, "Link: ")
-        canvas.drawString(20, 700, url)
-        rect = Canvas.rect(canvas, 20, 700, 400, 20, 0)
-        canvas.linkURL(url, rect)
-        canvas.setFont("Helvetica", 11)
-        canvas.save()
 
 if __name__ == '__main__':
 
