a960fe8087469133412fe89bd24cee8e40569fe9
SuleyNL <Suleyleeuw@gmail.com>
2021-10-04 18:13:11 +0200

    added comments to GoogleScraper
    added decryption to PDFScraper
    
    streamlined code of PDFScraper
    
    first and last 100 bytes are added to reportpage
    
    extra EOFerrors have been caught in PDFScraper
    
    DataScraper now has Category class and can categorise all organisations with relevant matrixes and lists

--- DataScraper.py
+++ DataScraper.py
@@ -9,33 +9,164 @@ import pikepdf
 from pdfminer.pdfpage import PDFPage
 from reportlab.pdfgen.canvas import Canvas
 from collections import Counter
+from PyDFScraper import Error
+from enum import Enum
 
-def startProcess(year):
-    PDFsList = getPDFsList(2020)
-    hashmap = Counter(PDFsList)
-    noresultsCounter = 0
-    resultsCounter = 0
-    for i in hashmap:
-        if i.endswith("noresults.pdf"):
-            noresultsCounter += hashmap.get(i)
 
-        if i.endswith(".pdf"):
-            resultsCounter = resultsCounter + hashmap.get(i)
+def createRapport(year):
+    global categories
+    global totalOrganisations
+    #: Initialise all variables
+    initialiseVariables(year)
 
-        else:
-            print(i + ":  " + str(hashmap.get(i)))
+    #: Categorise all organisations into the different search categories:
+    #: [Wel resultaten,
+    #:  Te weinig resultaten,
+    #:  Encrypted,
+    #:  EOF-Error,
+    #:  Geen resultaten,
+    #:  Geen document]
+    categorise()
+    '''
+    #: print all info from category
+    for category in categories:
+    print("Categorie " + category.name)
+    print(category.amount)
+    print(category.matrix)
+    '''
 
-    print("results: " + ":  " + str(resultsCounter))
-    print("noresults: " + ":  " + str(noresultsCounter))
+    for category in categories:
+        print(category.name + ": " + calculatePercentage(category.amount, totalOrganisations))
 
-    print("percentage of noresults: " + str((round(noresultsCounter/resultsCounter, 3))*100) + "%")
+    #print()
+    print(totalOrganisations)
+    #print(categories[2].name)
+    #print(categories[2].organisations)
+
     return "------[THE END]------"
 
 
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.count = 0
+        self.matrix = []
+        self.organisations = []
+
+
+class SearchTerm(Enum):
+    succes = "Wel resultaten"
+    fewresults = "Te weinig resultaten"
+    encryption = "Encrypted"
+    EOF = "EOF-Error"
+    noresults = "Geen resultaten"
+    nodocument = "Geen document"
+
+
+def initialiseVariables(currentYear):
+    global year
+    year = currentYear
+
+    global organisationsList
+    organisationsList = getOrganisationsList(year)
+
+    global PDFsListRaw
+    PDFsListRaw = getPDFsList(year)
+
+    global PDFsListClean
+    PDFsListClean = []
+    for i in PDFsListRaw:
+        PDFsListClean.append(i.split(".")[1].strip())
+
+    global processedData
+    processedData = Counter(PDFsListClean)
+
+    global path
+    path = "PDFs/{0}/".format(year)
+
+    global categories
+    categories = []
+    for searchterm in SearchTerm:
+        categories.append(Category(searchterm.value))
+
+    global totalPdfs
+    totalPdfs = 0
+    for item in processedData:
+        itemAmount = processedData.get(item)
+        totalPdfs += itemAmount
+
+    global totalOrganisations
+    totalOrganisations = len(os.listdir(path))
+
+
+def categorise():
+    global categories
+    #: Create an empty matrix to pass through the initial matrixAtLeastOne() method
+    matrix = createEmptyMatrix()
+
+    #: Convert the un-indexable SearchTerm Enum into an indexable List
+    searchTermList = [e for e in SearchTerm]
+    searchTermListCount = len(searchTermList)
+
+    #: Pass every searchTerm through the matrixAtLeastOne() checker
+    #: To check if there is at least one Pdf in the organisation that meets the criteria
+    #: This will determine in which classification the organisation goes
+    i = 0
+    while i < searchTermListCount:
+        #: Every searchTerm will produce a more saturated matrix
+        #: Every searchTerm will use the matrix of the searchTerm before it
+        searchData = matrixAtLeastOne(matrix, searchTermList[i])
+        matrix = searchData[0]
+        categories[i].matrix = searchData[1]
+        categories[i].amount = searchData[2]
+        categories[i].organisations = searchData[3]
+        i += 1
+
+
+def matrixAtLeastOne(filledMatrix, searchTerm):
+    global path
+    global year
+    global PDFsListClean
+    global organisationsList
+    #: Create variables for loop;
+    #: - searchTermCount for how often the searchterm occurs at least once in the available matrix
+    #: - totalOrganisations we loop through all available organisations
+    #: - UniqueMatrix is the matrix that will correspond to this specific category. It will only on have a '1' on
+    #:   the places where an instance of an organisation belonging to this category is found
+    searchTermCount = 0
+    totalOrganisations = len(os.listdir(path))
+    UniqueMatrix = createEmptyMatrix()
+    UniqueOrganisationsList = []
+    i = 0
+
+    while i < totalOrganisations:
+        if not filledMatrix[i]:
+            #: If there is a 0 it means that organisation hasn't been categorised yet
+            currentPDF = i*3
+            if searchTerm.value in PDFsListClean[currentPDF:currentPDF + 3]:
+                #: Check if this current searchTerm in one of the 3 Pdf's of organisation
+                filledMatrix[i] = 1
+                #: Fill in the filledMatrix; this org has been classified
+                UniqueMatrix[i] = 1
+                #: Fill in the uniqueMatrix; this is the classified organisation
+                UniqueOrganisationsList.append(organisationsList[i])
+                #: Add the organisation name to the UniqueOrganisationsList, this will be saved in its corresponding category
+                searchTermCount += 1
+                #: Increment total found searches on this searchTerm
+        i += 1
+
+    return filledMatrix, UniqueMatrix, searchTermCount, UniqueOrganisationsList
+
+
+def calculatePercentage(partial, total):
+    #: calculates the percentage of organisations in each category
+    percentage = str(round(100 * (partial / total))) + "%"
+    return percentage
+
+
 def getPDFsList(year):
-    coordinates = [0, 0]
     PDFsList = []
-    path = "PDFs/%s/" %year
+    path = "PDFs/%s/" % year
     pdfCounter = 0
     Path(path).mkdir(parents=True, exist_ok=True)
 
@@ -47,6 +178,28 @@ def getPDFsList(year):
     return PDFsList
 
 
+def createEmptyMatrix():
+    emptyMatrix = []
+    i = 0
+    while i < totalOrganisations:
+        emptyMatrix.append(0)
+        i += 1
+    return emptyMatrix
+
+
+def getOrganisationsList(year):
+    organisationsList = []
+    path = "PDFs/%s/" % year
+    organisationCounter = 0
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    for organisation in os.listdir(path):
+            organisationCounter += 1
+            organisationsList.append(organisation)
+
+    return organisationsList
+
+
 if __name__ == '__main__':
 
-    print(startProcess(2020))
+    print(createRapport(2020))
--- PyDFScraper.py
+++ PyDFScraper.py
@@ -10,6 +10,8 @@ import pikepdf
 from pdfminer.pdfpage import PDFPage
 from reportlab.pdfgen.canvas import Canvas
 from enum import Enum
+import warnings
+
 
 def startProcess(year):
     global isError
@@ -29,11 +31,18 @@ def startProcess(year):
 
         if not isError and isFileDownloaded(url):
             try:
-                pageNumber = getPageNumber()
+                if isEncrypted():
+                    errorhandler(Error.encryptionError)
+                else:
+                    pageNumber = getPageNumber()
+
             except PyPDF2.utils.PdfReadError as e:
                 errorhandler(Error.EOFError)
 
         generateFile(pageNumber, year, organisation, iteration, url)
+        print("Processed {0} organisations and {1} files".format(iteration[0]+1, ((iteration[0]-1)*3)+iteration[1]))
+        newPath = Path("PDFs/{0}/{1}/".format(year, organisation))
+
     return "------[THE END]------"
 
 
@@ -55,7 +64,7 @@ def errorhandler(error):
 def getIteration(year):
     coordinates = [0, 0]
 
-    path = "PDFs/%s/" %year
+    path = "PDFs/{0}/".format(year)
     pdfCounter = 0
     Path(path).mkdir(parents=True, exist_ok=True)
 
@@ -87,7 +96,7 @@ def getOrganisation(iteration):
 def getUrl(coordinates, year):
     url = ""
     # if it doesnt create the file
-    #filename.mkdir(parents=True, exist_ok=True)
+    # filename.mkdir(parents=True, exist_ok=True)
 
     with open("PDFs-List/PDFs-List-%s.txt" % year) as file:
         line = file.readlines()[coordinates[0]].split(":")
@@ -95,8 +104,11 @@ def getUrl(coordinates, year):
 
         urlList = ":".join(line).split(", ")
 
-        if not coordinates[1] == len(urlList):
-            url = ":".join(line).split(", ")[coordinates[1]].strip()
+        #: if the current iteration (goes from 0,1,2) is smaller than the amount of urls in the line (could be 0,1,2,3)
+        #: that means we can get a document
+        #: else we raise nodocumentError
+        if coordinates[1] < len(urlList):
+                url = ":".join(line).split(", ")[coordinates[1]].strip()
         else:
             errorhandler(Error.nodocumentError)
     return url
@@ -105,51 +117,105 @@ def getUrl(coordinates, year):
 def isFileDownloaded(url):
 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"}
+    i = 0
+    while i < 10:
+        # try to download file to workingmemory
+        try:
+            print("waiting up to 1 minute to download pdf from: {0}".format(url))
+            response = requests.get(url, headers=headers, timeout=60)
+            path = Path("WorkingMemory")
+            path.mkdir(parents=True, exist_ok=True)
+            with open("WorkingMemory/currentFile.pdf", "wb+") as file:
+                file.write(response.content)
+            return True
+        # if connection is lost, we wait and try gain
+        except requests.exceptions.ConnectionError:
+            print("error: connectionerror")
+            print("sleeping {0} sec then trying again".format((i+1)*10))
+            time.sleep((i+1)*10)
+            i += 1
+            break
+        except UnboundLocalError:
+            print("error: unboundlocal error")
+            print("trying again")
+            i += 1
+            break
+    return False
 
-    try:
-        print("waiting up to 1 minute to download pdf from: %s" % url)
-        response = requests.get(url, headers=headers, timeout=60)
+
+def isEncrypted():
+    pdf = PdfFileReader("WorkingMemory/currentFile.pdf")
+
+    if pdf.isEncrypted:
+    # if it is encrypted, try to decrypt it
+        try:
+            pdf1 = pikepdf.open("WorkingMemory/currentFile.pdf", allow_overwriting_input=True)
+            pdf1.save('WorkingMemory/currentFile.pdf')
+            pdf1.close()
+
+            EOF_MARKER = b'%%EOF'
+
+            mainfile = "WorkingMemory/currentFile.pdf"
+            temporaryfile = "WorkingMemory/temporaryfile.pdf"
+            with open(mainfile, 'rb') as f:
+                contents = f.read()
+                
+                # check if EOF is somewhere else in the file
+                if EOF_MARKER in contents:
+                    # we can remove the early %%EOF and put it at the end of the file
+                    contents = contents.replace(EOF_MARKER, b'')
+                    contents = contents + EOF_MARKER
+                else:
+                    # Some files really don't have an EOF marker
+                    # In this case it helped to manually review the end of the file
+                    print(contents[-8:])  # see last characters at the end of the file
+                    # printed b'\n%%EO%E'
+                    contents = contents + EOF_MARKER
+
+                with open(temporaryfile, 'wb') as g:
+                    g.write(contents)
+
+                infile = PdfFileReader(temporaryfile, False)
+                output = PdfFileWriter()
+
+                i = 0
+                while i < infile.getNumPages():
+                    p = infile.getPage(i)
+                    output.addPage(p)
+                    i += 1
+
+                with open(mainfile, 'wb') as h:
+                    output.write(h)
 
 
-        path = Path("WorkingMemory")
-        path.mkdir(parents=True, exist_ok=True)
+        except:
+            return True
+            print("errortje")
+            # https://smallpdf.com/unlock-pdf is an alternate method for decryption
+            # OCR is another alternative
+    # if it is still encrypted, return true, else return false
 
-        with open("WorkingMemory/currentFile.pdf", "wb+") as file:
-            file.write(response.content)
-
-    except requests.exceptions.ConnectionError or UnboundLocalError:
-        print("error: ")
-        print("trying again")
-        isFileDownloaded(url)
-    return True
+    if pdf.isEncrypted:
+        return True
+    else:
+        return False
 
 
 def getPageNumber():
     pageNumber = 0
     pdf = PdfFileReader("WorkingMemory/currentFile.pdf")
 
-    if pdf.isEncrypted:
-        try:
-            pdf1 = pikepdf.open("WorkingMemory/currentFile.pdf", allow_overwriting_input=True)
-            pdf1.save()
-            pdf1.close()
-            #pdf = pdf.decrypt('')
-        except NotImplementedError:
-            print("errortje")
-            # https://smallpdf.com/unlock-pdf is an alternate method for decryption
-            # OCR is another alternative
-
     totalScore = []
 
     i = 0
     try:
+        # For every page, check the amount of occurences of these searchterms
         while i < pdf.getNumPages():
             page = pdf.getPage(i).extractText().split(" ")
 
             #relevanceScore = page.count("bezoldiging") + page.count("Bezoldiging") + page.count("BEZOLDIGING") + page.count("WNT")
-            #print(item.count("bezoldiging") for item in page)
             highscore = 0
-            currentScore =  sum((itm.count("bezoldiging") for itm in page)) + \
+            currentScore = sum((itm.count("bezoldiging") for itm in page)) + \
                             sum((itm.count("WNT") for itm in page)) + \
                             sum((itm.count("bezoldigingsmaximum") for itm in page)) + \
                             sum((itm.count("Wet Normering") for itm in page)) + \
@@ -160,7 +226,7 @@ def getPageNumber():
                 highscore = currentScore
                 pageNumber = highscore
             i += 1
-
+            print(str(pdf.getNumPages()-i))
     except:
         errorhandler(Error.encryptionError)
 
@@ -172,8 +238,8 @@ def getPageNumber():
     else:
         # Find the page number with the highest value
         pageNumber = totalScore.index(max(totalScore))
-    print(totalScore)
-    print(pageNumber)
+    print("Relevance score matrix: \n" + str(totalScore))
+    print("Relevant page: " + str(pageNumber))
     return pageNumber
 
 
@@ -182,7 +248,7 @@ def generateFile(pageNumber, year, organisation, iteration, url):
     global report
 
     #   create the file
-    newPath = Path("PDFs/{0}/{1}/".format(year, organisation))
+    newPath = Path("PDFs/{0}/{1} - {2}/".format(year, iteration[0], organisation))
     newPath.mkdir(parents=True, exist_ok=True)
     newFile = str(newPath) + "/{0}. {1}.pdf".format(iteration[1]+1, report)
 
@@ -221,23 +287,56 @@ def generateFile(pageNumber, year, organisation, iteration, url):
     rect = Canvas.rect(canvas, 20, 700, 400, 20, 0)
     canvas.linkURL(url, rect)
 
+    with open("WorkingMemory/currentFile.pdf", 'rb') as f:
+        contents = f.read()
+        rawdata1 = contents[:50]
+        rawdata2 = contents[50:100]
+        rawdata3 = contents[-100:-50]
+        rawdata4 = contents[-50:-1]
+
+    canvas.drawString(20, 620, "Eerste 100 bytes: ")
+    canvas.drawString(20, 600, str(rawdata1))
+    canvas.drawString(20, 580, str(rawdata2))
+
+    canvas.drawString(20, 550, "Laatste 100 bytes: ")
+    canvas.drawString(20, 530, str(rawdata3))
+    canvas.drawString(20, 510, str(rawdata4))
+
+    canvas.drawString(20, 480, "ID: ")
+    canvas.drawString(180, 480, "Organisatie: " + str(iteration[0]+1) + ", Document: " +str(iteration[1]+1))
+
     canvas.setFont("Helvetica", 11)
     canvas.save()
 
-    # if there is no error: we have located the relevant pages and can store them in this pdf
+    #: if there is no error: we have located the relevant pages and can store them in this pdf
     if not isError:
-        pages_to_keep = [pageNumber-1, pageNumber, pageNumber+1, pageNumber+2]
-        infile = PdfFileReader("WorkingMemory/currentFile.pdf", 'rb')
-        output = PdfFileWriter()
+        #: Sometimes a pdf with a EOF-Error still gets through, we catch it with this try-block
+        try:
+            infile = PdfFileReader("WorkingMemory/currentFile.pdf", strict=False)
+        except PyPDF2.utils.PdfReadError:
+            #: if there is an EOF-error 'isError' will be set to True in the errorhandler
+            errorhandler(Error.EOFError)
 
-        reportpage = PdfFileReader(newFile, 'rb').getPage(0)
-        output.addPage(reportpage)
-        for i in pages_to_keep:
-            p = infile.getPage(i)
-            output.addPage(p)
+        if not isError:
+            output = PdfFileWriter()
+            i = pageNumber-1
 
-        with open(newFile, 'wb') as f:
-            output.write(f)
+            # first add the generated report to the new pdf file
+            reportPage = PdfFileReader(newFile, 'rb').getPage(0)
+            output.addPage(reportPage)
+
+            # then check if there is pages left to add, starting at one before the main page, up to 4
+            j = 0
+            print(i)
+            while i < infile.getNumPages() and j < 4:
+                p = infile.getPage(i)
+                output.addPage(p)
+                i += 1
+                j += 1
+
+            with open(newFile, 'wb') as f:
+                output.write(f)
+
 
 
 if __name__ == '__main__':
